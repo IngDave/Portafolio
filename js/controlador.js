b93d0f68bfedccfb7a8205d4439adb14d38e4665
david tejada <daves.dev@outlook.com>
2024-08-05 03:16:32 -0500

    Summary section Services
    se  agregan los servicios al portafolio

---
+++ js/controlador.js
@@ -0,0 +1,28 @@
+/*animador texto section about*/
+const div = document.querySelector(".about__script__Texto"),
+    about__script__Texto = "{ Ingeniero de Software }";
+
+function efectoTextTyping(elemento, texto, i = 0) {
+    elemento.textContent += texto[i];
+
+    if (i === about__script__Texto.length - 1) return;
+
+    setTimeout(() => efectoTextTyping(div, about__script__Texto, i + 1), 120);
+
+}
+efectoTextTyping(div, about__script__Texto);
+
+/*Alert_Formulario*/
+document.getElementById('btn_Submit').onclick = function () {
+    Swal.fire({
+        position: "bottom-end",
+        icon: "success",
+        title: "Mensaje enviado",
+        text: "¡Guardado exitosamente!",
+        showConfirmButton: false,
+        timer: 2500,
+        timerProgressBar: true,
+        backdrop: true,
+        toast: true
+    })
+}
