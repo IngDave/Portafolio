19323752cbdd8487807180fb4cdc8fd7a9aa45dc
David Tejada <daves.dev@outlook.com>
2024-09-28 17:10:23 -0500

    Update

--- js/controlador.js
+++ js/controlador.js
@@ -1,5 +1,5 @@
 /*animador texto section about*/
-const div = document.querySelector(".about__script__Texto"),
+const div = document.querySelector(".container__about__script__Texto"),
     about__script__Texto = "{ Ingeniero de Software }";
 
 function efectoTextTyping(elemento, texto, i = 0) {
@@ -11,6 +11,20 @@ function efectoTextTyping(elemento, texto, i = 0) {
 
 }
 efectoTextTyping(div, about__script__Texto);
+
+//evento que permite abrir campos de texto:
+const mostrarTexto = document.getElementById('mostrar__parrafo');
+const btn = document.getElementById('.container__services__btn');
+
+btn.addEventListener('click', function() {
+    if (mostrarTexto.style.display === 'none' || mostrarTexto.style.display === '') {
+        mostrarTexto.style.display = 'block';
+        btn.textContent = 'Ocultar párrafo';
+    } else {
+        mostrarTexto.style.display = 'none';
+        btn.textContent = 'Mostrar párrafo';
+    }
+});
 
 /*Alert_Formulario*/
 document.getElementById('btn_Submit').onclick = function () {
@@ -26,3 +40,4 @@ document.getElementById('btn_Submit').onclick = function () {
         toast: true
     })
 }
+
